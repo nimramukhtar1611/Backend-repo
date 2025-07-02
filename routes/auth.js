436fa2ca78c72fe4ef6e2960ea2d07234b6b141c
nimra <snimramukhtar321@gmail.com>
2025-07-02 23:20:29 +0500

    code update

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const adminrouter = express.Router();
-const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 adminrouter.post("/login", async (req, res) => {
@@ -9,7 +8,7 @@ adminrouter.post("/login", async (req, res) => {
   const adminUsername = "admin@restaurantevgo.com";
   const adminPassword = "Hygensdreadcup200";
 
-  if (username !== adminUsername) return res.status(401).json({ msg: "Invalid Email" });
+  if (username !== adminUsername) return res.status(401).json({ msg: "Invalid username" });
 
   if (password !== adminPassword) return res.status(401).json({ msg: "Invalid Password" });
 
